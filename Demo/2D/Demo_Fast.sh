7043f85ba84eeb355187ce01ffe293f8d75f8714
Charles A Bouman <Charles.Bouman@gmail.com>
2019-07-29 18:38:06 -0400

    Adds Reconstruction folder using gitignore

--- Demo/2D/Demo_Fast.sh
+++ Demo/2D/Demo_Fast.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cd "$(dirname $0)"
+
 echo " #================================================================#"
 echo " #    DEMO : 2D PARALLEL-BEAM CT MBIR Reconstruction SOFTWARE     #"
 echo " # by Pengchong Jin, Xiao Wang,  Venkatesh Sridhar and C.A.Bouman #"
