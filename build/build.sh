be3e04a6246f4e25c248e3dbe388fefba1ba5b8d
Thilo Balke <thilo.balke@gmail.com>
2019-07-30 16:13:10 -0400

    Removes old makefiles and compile scripts

--- build/build.sh
+++ build/build.sh
@@ -4,25 +4,27 @@ cd "$(dirname $0)"
 
 FLAGS="-Wall -pedantic -lm"
 
+# libs
 SOURCES=""
-SOURCES="${SOURCES} ../SourceCode/2D/MBIR/mbir_2D.c"
 SOURCES="${SOURCES} ../SourceCode/2D/MBIR/icd_2D.c"
 SOURCES="${SOURCES} ../SourceCode/2D/MBIR/initialize_2D.c"
 SOURCES="${SOURCES} ../SourceCode/2D/MBIR/recon_2D.c"
 SOURCES="${SOURCES} ../SourceCode/2D/Generate_SystemMatrix/A_comp_2D.c"
-SOURCES="${SOURCES} ../SourceCode/2D/Generate_SystemMatrix/Gen_SysMatrix_2D.c"
-
 SOURCES="${SOURCES} ../SourceCode/3D/MBIR/initialize_3D.c"
 SOURCES="${SOURCES} ../SourceCode/3D/MBIR/icd_3D.c"
 SOURCES="${SOURCES} ../SourceCode/3D/MBIR/recon_3D.c"
-SOURCES="${SOURCES} ../SourceCode/3D/MBIR/mbir_3D.c"
-SOURCES="${SOURCES} ../SourceCode/3D/Generate_SystemMatrix/Gen_SysMatrix_3D.c"
 SOURCES="${SOURCES} ../SourceCode/3D/Generate_SystemMatrix/A_comp_3D.c"
-
 SOURCES="${SOURCES} ../SourceCode/allocate.c"
 SOURCES="${SOURCES} ../SourceCode/MBIRModularUtils_2D.c"
 SOURCES="${SOURCES} ../SourceCode/MBIRModularUtils_3D.c"
+SOURCES="${SOURCES} ../SourceCode/MBIRModularUtils_3D.c"
+SOURCES="${SOURCES} ../SourceCode/MBIRModularUtils_3D.c"
 
+# mains
+SOURCES="${SOURCES} ../SourceCode/2D/MBIR/mbir_2D.c"
+SOURCES="${SOURCES} ../SourceCode/2D/Generate_SystemMatrix/Gen_SysMatrix_2D.c"
+SOURCES="${SOURCES} ../SourceCode/3D/MBIR/mbir_3D.c"
+SOURCES="${SOURCES} ../SourceCode/3D/Generate_SystemMatrix/Gen_SysMatrix_3D.c"
 gcc $FLAGS -c $SOURCES
 
 
@@ -35,11 +37,6 @@ gcc $FLAGS $UTILS icd_3D.o initialize_3D.o recon_3D.o mbir_3D.o    -o mbir_3D.bi
 
 rm *.o
 mv *.bin ../bin
-
-
-
-
-
 
 
 
